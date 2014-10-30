bd690acb9d78255f37092f00911f2ba4306b579e
Justin Chang <justin.chang@blackbaud.com>
2014-10-30 13:55:12 -0500

    adds logic to turn on light if it is not working.

--- bin/update_device.rb
+++ bin/update_device.rb
@@ -1,4 +1,21 @@
-require 'jenkins_api_client'
 require 'active_support'
-require 'active_support/inflector'
+require 'jenkins_api_client'
+require 'yaml'
+
+PROJECT_FILE_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+yaml_file = File.join PROJECT_FILE_PATH, 'config.yml'
+config = YAML.load_file(yaml_file)
+
+@jenkins = JenkinsApi::Client.new(:server_ip => "jenkins-qa.blackbaudcloud.com", :server_port => 80, :username => config['username'], :password => config['password'])
+
+failed = false
+@jenkins.api_get_request('/view/Peer to Peer/view/Smoke Test')['jobs'].each do |job|
+  if job['color'] != 'blue'
+    failed = true
+    break
+  end
+end
+
+status = failed ? 'on' : 'off'
+`heyu #{status} a2`
 
